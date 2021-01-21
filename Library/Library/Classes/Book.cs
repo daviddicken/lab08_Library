6dca4b0f1898511e09b43354d577840d3fcde4e2
daviddicken <davidfromseattle@gmail.com>
2021-01-20 20:40:53 -0800

    finished code for lab 8

---
+++ Library/Library/Classes/Book.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library
+{
+    class Book
+    {
+        public String Title { get; set; }
+        public Author Author { get; set; }
+        public Genre Genre { get; set; }
+        
+
+        public Book() { }
+        public Book(String title, String authorFirst, String authorLast, Genre genre)
+        {
+            Author = new Author(authorFirst, authorLast);
+            Title = title;
+            Genre = genre;          
+        }
+        public Book(String title, Author author, Genre genre)
+        {
+            Author = author;
+            Title = title;
+            Genre = genre;
+        }
+
+
+
+    }
+
+    enum Genre
+    {
+        SciFi,
+        Fiction,
+        Childrens,
+        Horror,
+        Romance
+    }
+}
---
+++ Library/Library/Classes/Library.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Classes
+{
+    public class Library<T> : IEnumerable
+    {
+        public T[] books = new T[5];
+        public int bookCount = 0;
+
+        public void Add(T book)
+        {
+            if(bookCount == books.Length)
+            {
+                Array.Resize(ref books, books.Length * 2);
+            }
+            books[bookCount++] = book;
+        }
+
+        //public void Print()
+        //{
+        ////    for(int i = 0; i < bookCount; i++)
+        ////        Console.WriteLine(books[i].);
+        //}
+
+        //iterate through array
+        //if book is found
+        // remove book
+        // decrement bookcounter
+        // move all following books up 1
+        public void Remove(T book)
+        {
+            bool flag = false;
+            
+            for(int i = 0; i < bookCount; i++)
+            {
+                if (books[i].Equals(book)) flag = true;
+                if (flag && i < books.Length - 1) books[i] = books[i + 1];
+            }
+            if (flag) bookCount--;
+        }
+        public int Count()
+        {
+            return bookCount;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for(int i = 0; i < bookCount; i++) 
+                yield return books[i];
+        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();       
+    }
+}
